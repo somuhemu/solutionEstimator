606f75ccd0aaba4d17616405bb934814fea2f3d4
somuhemu <somusomendra321@gmail.com>
2021-10-28 23:43:42 +0530

    secondcommit

--- backend/api/submission/controllers/submission.js
+++ backend/api/submission/controllers/submission.js
@@ -6,24 +6,24 @@
  */
 
 module.exports = {
-    create : async ctx =>{
-		console.log(ctx.request.body);
-		var data=ctx.request.body;
-		var questions=data.answers;
-		var lowerEstimate=0;
-		var upperEstimate=0;
-		for(var i=0;i<questions.length;i++){
-			for(var j=0;j<questions[i].options.length;j++){
-console.log(questions[i].options[j]);
-				lowerEstimate+=questions[i].options[j].minPrice;
-				upperEstimate+=questions[i].options[j].maxPrice;
+	//api customization
+	create: async ctx => {
+		let data = ctx.request.body;
+		let answers = data.answers;
+		let lowerPrice = 0;
+		let upperPrice = 0;
+		for (var i = 0; i < answers.length; i++) {
+			for (var j = 0; j < answers[i].options.length; j++) {
+				// console.log(answers[i].options[j]);
+				lowerPrice += answers[i].options[j].minPrice;
+				upperPrice += answers[i].options[j].maxPrice;
 			}
 		}
-		data.lowerEstimate=lowerEstimate;
-		data.upperEstimate=upperEstimate;
-		console.log(data);
-		var entity = await strapi.services.submission.create(data);
-		return entity
+		data.lowerEstimate = lowerPrice;
+		data.upperEstimate = upperPrice;
+		let values = await strapi.services.submission.create(data);
+		console.log(values)
+		return values
 
 	}
     
